890498c8f776112b7219f2bb69fa0e581feb53a1
efra8ab <efra8ab@gmail.com>
2025-07-11 16:32:03 -0600

    Dynamic arrays exercises completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Add our dynamic array
+        List<double> multiples = new List<double>();
+
+        //A for loop will help us to add each multiple to our array
+
+        for (int i = 1; i <= length; i++)
+        {
+            multiples.Add(number * i);
+        }
+
+        //lastly we convert to a static array as indicated by the function output
+        return multiples.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,26 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //flipping the list 
+        Reverse(data, 0, data.Count - 1);
+        //flipping the first values (previously the last ones)
+        Reverse(data, 0, amount - 1);
+        //flipping the rest of the values
+        Reverse(data, amount, data.Count - 1);
+
+    }
+
+    public static void Reverse(List<int> list, int start, int end)
+    {
+        //keep it open while our start is less than our last value 
+        while (start < end)
+        {
+            int temp = list[start]; //saving the value of the starting index in temp for later
+            list[start] = list[end]; // we assign the Start place the last value (from our last place)
+            list[end] = temp; // we now assign to the last position the first value we saved on temp
+            start++; //increase the start place by 1 until we reach the middle
+            end--; // decrease the last place by 1 until we reach the middle
+        }
     }
 }
