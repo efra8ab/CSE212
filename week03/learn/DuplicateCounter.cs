5cca38be76d1c344efa04b6d0ad72575d168b60e
efra8ab <efra8ab@gmail.com>
2025-07-31 01:32:11 -0600

    duplicate counters and translator

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,6 @@
-﻿public class DuplicateCounter
+﻿using System.Globalization;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +26,21 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var mySet = new HashSet<int>();
+        int duplicates = 0;
+
+        foreach (int num in data)
+        {
+            if (mySet.Contains(num))
+            {
+                duplicates++;
+            }
+            else
+            {
+                mySet.Add(num);
+            }
+        }
+
+        return duplicates;
     }
 }
